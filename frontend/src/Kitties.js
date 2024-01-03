9e89bfec438e7c9f42da649f31da198566db8466
tin-snowflake <cqtin0903@gmail.com>
2024-01-03 19:14:40 +0700

    3-mint-kitty

---
+++ frontend/src/Kitties.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react'
+import { Form, Grid } from 'semantic-ui-react'
+
+import { useSubstrateState } from './substrate-lib'
+import { TxButton } from './substrate-lib/components'
+
+import KittyCards from './KittyCards'
+
+const parseKitty = ({ dna, price, gender, owner }) => ({
+  dna,
+  price: price.toJSON(),
+  gender: gender.toJSON(),
+  owner: owner.toJSON(),
+})
+
+function toHexString(byteArray) {
+  var s = '0x'
+  byteArray.forEach(function (byte) {
+    s += ('0' + (byte & 0xff).toString(16)).slice(-2)
+  })
+  return s
+}
+
+export default function Kitties(props) {
+  const { api, keyring } = useSubstrateState()
+  const [kitties, setKitties] = useState([])
+  const [status, setStatus] = useState('')
+
+  const subscribeCount = () => {
+    let unsub = null
+
+    const asyncFetch = async () => {
+      unsub = await api.query.kitties.allKittiesCount(async count => {
+        // Fetch all kitty keys
+        const entries = await api.query.kitties.kitties.entries()
+        const kittiesMap = entries.map(entry => {
+          return {
+            id: toHexString(entry[0].slice(-32)),
+            ...parseKitty(entry[1].unwrap()),
+          }
+        })
+        setKitties(kittiesMap)
+      })
+    }
+
+    asyncFetch()
+
+    return () => {
+      unsub && unsub()
+    }
+  }
+
+  useEffect(subscribeCount, [api, keyring, kitties])
+
+  return (
+    <Grid.Column width={16}>
+      <h1>Kitties</h1>
+      <KittyCards kitties={kitties} setStatus={setStatus} />
+      <Form style={{ margin: '1em 0' }}>
+        <Form.Field style={{ textAlign: 'center' }}>
+          <TxButton
+            label="Create Kitty"
+            type="SIGNED-TX"
+            setStatus={setStatus}
+            attrs={{
+              palletRpc: 'kitties',
+              callable: 'createKitty',
+              inputParams: [],
+              paramFields: [],
+            }}
+          />
+        </Form.Field>
+      </Form>
+      <div style={{ overflowWrap: 'break-word' }}>{status}</div>
+    </Grid.Column>
+  )
+}
